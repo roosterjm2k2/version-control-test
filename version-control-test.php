27c7dfedd75b91da96ff5eb6b25ce7c1df122a53
Joshua McDonald <me@joshuamcdonald.info>
2014-03-07 17:37:03 -0600

    post recieve hook wired up

---
+++ version-control-test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Version Control Test for Uptrending
+ * Plugin URI: http://www.joshuamcdonald.info
+ * Description: A version control plugin test, because you want me working for you!
+ * Version: 0.1
+ * Author: Joshua McDonald
+ * Author URI: http://www.joshuamcdonald.info
+ * License: GPL2
+ */
+
+ 
