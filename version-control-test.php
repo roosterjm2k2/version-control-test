582658dcf8215c8978539a67e69521dbe663e24e
Joshua McDonald <me@joshuamcdonald.info>
2014-03-07 18:43:06 -0600

    Added meta box to admin to select version

--- version-control-test.php
+++ version-control-test.php
@@ -16,4 +16,34 @@ function vct_display_version() {
  	print "<span class=\"version\">Version $version";
 }
 
+add_action('admin_init','vct_meta_init');
+ 
+// Add the meta box to posts and pages
+function vct_meta_init() {
+	add_meta_box('vct_meta', 'Version Control', 'vct_meta_setup', 'page', 'side', 'high');
+	add_meta_box('vct_meta', 'Version Control', 'vct_meta_setup', 'post', 'side', 'high');
+    add_action('save_post','vct_meta_save');
+}
+
+// Build out the meta-box for the version control admin panel for pages 
+function vct_meta_setup() {
+    global $post;
+    $meta = get_post_meta($post->ID,'_vct_version',TRUE);
+ 	$commits = get_option('vct_commit_count');
+ 	echo '<label>Applies to Version:</label> <select name="_vct_version"><option value="">Current</option>';
+ 	for($i=1; $i<=$commits; $i++) {
+ 		$selected = $meta == $i ? ' selected="selected"' : '';
+ 		echo '<option value="'.$i.'"'.$selected.'>'.($i / 10).'</option>';
+ 	}
+ 	echo '</select>';
+}
+
+// Update per-post version on admin form save
+function vct_meta_save() {
+	global $post;
+	$post_id = $post->ID;
+	$new_value = $_POST['_vct_version'];
+	update_post_meta($post_id,'_vct_version',$new_value);
+}
+
 ?>
